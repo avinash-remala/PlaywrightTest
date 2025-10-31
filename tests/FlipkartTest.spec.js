b0349d90dc056239fb88c66f745c1a119af68dc4
avinash <avinash.remala@gmail.com>
2025-10-30 23:01:51 -0500

    test

--- tests/FlipkartTest.spec.js
+++ tests/FlipkartTest.spec.js
@@ -5,3 +5,9 @@ test('Flipkart Test', async ({ page }) => {
   await page.locator("(//a[@class='_3490ry']/following-sibling::a)[3]").click();
   await expect(page.getByRole('heading', { name: 'Formal Shirts', exact: true })).toBeVisible();
 });
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.flipkart.com/');
+  await page.getByRole('link', { name: 'Men\'s Formal Shirts' }).click();
+  await expect(page.getByRole('heading', { name: 'Formal Shirts', exact: true })).toBeVisible();
+});
