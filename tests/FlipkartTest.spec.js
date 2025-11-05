b760d1dd95fdbb74d8ba063f0c6aca6185918614
avinash <avinash.remala@gmail.com>
2025-11-04 21:07:01 -0600

    flipkartchanges

--- tests/FlipkartTest.spec.js
+++ tests/FlipkartTest.spec.js
@@ -1,13 +1,10 @@
 import { test, expect } from '@playwright/test';
 
-test('Flipkart Test', async ({ page }) => {
+test('Flipkart Test Navigate to Formal Shirts', async ({ page }) => {
   await page.goto("https://www.flipkart.com/");
+  await page.waitForTimeout(20000);
+  await page.locator('//div[@aria-label="Fashion"]').hover();
   await page.locator("(//a[@class='_3490ry']/following-sibling::a)[3]").click();
   await expect(page.getByRole('heading', { name: 'Formal Shirts', exact: true })).toBeVisible();
-});
-
-test('test', async ({ page }) => {
-  await page.goto('https://www.flipkart.com/');
-  await page.getByRole('link', { name: 'Men\'s Formal Shirts' }).click();
-  await expect(page.getByRole('heading', { name: 'Formal Shirts', exact: true })).toBeVisible();
+  await page.close();
 });
